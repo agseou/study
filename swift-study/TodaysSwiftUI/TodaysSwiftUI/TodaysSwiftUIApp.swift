69375731882f4ba14264d42a466fd57d17549426
agseou <tjdnfkd1225@naver.com>
2023-05-29 10:49:19 +0900

    TodaysSwiftUI 프로젝트 생성

---
+++ swift-study/TodaysSwiftUI/TodaysSwiftUI/TodaysSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodaysSwiftUIApp.swift
+//  TodaysSwiftUI
+//
+//  Created by 은서우 on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct TodaysSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
